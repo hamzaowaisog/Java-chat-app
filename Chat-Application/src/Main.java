748a6e6597fd740b7e31ef749bcd834c53b5735c
Mir Hamza <122245138+hamzaowaisog@users.noreply.github.com>
2024-04-14 20:29:59 +0500

    Done with home gui designing

--- Chat-Application/src/Main.java
+++
@@ -1,6 +0,0 @@
-import GUI.*;
-public class Main {
-    public static void main(String[] args) {
-        mainframe mf = new mainframe();
-    }
-}
---
+++ chat-app/chat-app/src/chat/app/component/MenuButton.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package chat.app.component;
+
+import javax.swing.JButton;
+
+/**
+ *
+ * @author MIR HAMZA
+ */
+public class MenuButton extends JButton {
+    public MenuButton(){
+        setContentAreaFilled(false);
+        
+    }
+}
